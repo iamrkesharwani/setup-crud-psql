baa41d86d0302cd4d4ad909ceb46f307559942c6
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-02-07 20:05:45 +0530

    add nodejs connection

--- nodejs-connection/index.js
+++ nodejs-connection/index.js
@@ -1,13 +1,21 @@
 import db from './db.js';
 
-const getAllUsers = async () => {
+const getAllUsers = async (id) => {
   try {
-    const result = await db.query('SELECT * FROM users');
-    console.log('--- Connection Successful ---');
-    console.table(result.rows);
+    const queryText = 'SELECT * FROM users WHERE id = $1';
+    const values = [id];
+
+    const result = await db.query(queryText, values);
+
+    if (result.rows.length === 0) {
+      console.log(`User with ID ${id} not found`);
+    } else {
+      console.log(`User ID ${id} data:`);
+      console.table(result.rows);
+    }
   } catch (error) {
     console.error('Connection error:', error.message);
   }
 };
 
-getAllUsers();
+getAllUsers(1);
